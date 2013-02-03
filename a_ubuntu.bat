74c7cba9fec647df67e3ed4fd98f844118749e74
IwabuchiKen <iwabuchi.k.2010@gmail.com>
2013-02-03 14:40:44 +0900

    Periodical

---
+++ a_ubuntu.bat
@@ -0,0 +1,24 @@
+#!/bin/bash
+##!/bin/sh
+
+#export PATH=$PATH:/home/iwabuchiken/ws/ws_eclipse/G_nbp_new
+#echo "New path set => /home/iwabuchiken/ws/ws_eclipse/G_nbp_new"
+
+#echo $PATH
+
+#echo cd ./ws
+
+#java /home/iwabuchiken/ws/ws_eclipse/Time_calculator/bin/p1/E_5_multiple_sources
+
+# On the character "&" => http://itpro.nikkeibp.co.jp/article/COLUMN/20060228/231027/
+echo /home/iwabuchiken/ws/ws_eclipse/TC2/build/jar/TC2.jar &
+/home/iwabuchiken/ws/ws_eclipse/TC2/build/jar/TC2.jar &
+
+echo /usr/bin/freemind &
+/usr/bin/freemind &
+
+echo /usr/bin/eclipse &
+/usr/bin/eclipse &
+
+
+
